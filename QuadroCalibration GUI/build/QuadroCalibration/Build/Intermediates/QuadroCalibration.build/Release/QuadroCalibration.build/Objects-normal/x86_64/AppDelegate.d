b83917143f5e272336cd88f1a706dad4c1e8b8d9
Mattef <mattoef@yahoo.de>
2014-09-29 00:58:06 +0200

    bereinigt

--- QuadroCalibration GUI/build/QuadroCalibration/Build/Intermediates/QuadroCalibration.build/Release/QuadroCalibration.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/AppDelegate.m \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/AppDelegate.h \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPortManager.h \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPort.h
--- QuadroCalibration GUI/build/QuadroCalibration/Build/Intermediates/QuadroCalibration.build/Release/QuadroCalibration.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/main.m
