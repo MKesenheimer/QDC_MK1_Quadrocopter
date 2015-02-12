5b70a58889900664d7731f983b487545c25349d3
Mattef <mattoef@yahoo.de>
2015-02-12 09:04:04 +0100

    Updates

--- QuadroCalibration GUI/build/QuadroCalibration/Build/Intermediates/QuadroCalibration.build/Release/QuadroCalibration.build/Objects-normal/x86_64/ORSSerialPort.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPort.m \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPort.h
--- QuadroCalibration GUI/build/QuadroCalibration/Build/Intermediates/QuadroCalibration.build/Release/QuadroCalibration.build/Objects-normal/x86_64/ORSSerialPortManager.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPortManager.m \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPortManager.h \
-  /Users/kesenheimer/Desktop/Quadrocopter/QDC_MK1_Quadrocopter/QuadroCalibration\ GUI/Sources/ORSSerialPort.h
